1ae9e694f3f5a53d99c24a469d96529ab8cc4583
Walter Fender <cheftako@gmail.com>
2013-01-12 20:25:59 -0800

    Fixed Player initialize method to not require the universe

--- sample/src/main/java/com/linkedin/domination/sample/SamplePlayer.java
+++ sample/src/main/java/com/linkedin/domination/sample/SamplePlayer.java
@@ -4,13 +4,14 @@ import com.linkedin.domination.api.Event;
 import com.linkedin.domination.api.Move;
 import com.linkedin.domination.api.Player;
 import com.linkedin.domination.api.Universe;
+import sun.jvmstat.perfdata.monitor.PerfIntegerMonitor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SamplePlayer implements Player
 {
-    Universe _universe = null;
+    private Integer _me = null;
 
     public SamplePlayer()
     {
@@ -18,13 +19,13 @@ public class SamplePlayer implements Player
     }
 
     @Override
-    public void initialize(Universe universe)
+    public void initialize(Integer playerNbr)
     {
-        _universe = universe;
+        _me = playerNbr;
     }
 
     @Override
-    public List<Move> makeMove(List<Event> events)
+    public List<Move> makeMove(Universe universe, List<Event> events)
     {
         return new ArrayList();
     }
